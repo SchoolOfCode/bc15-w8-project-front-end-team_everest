bf5e860963ef78a01782666dfbf8c548d21befe0
Cal Woodford <callum@woodford.info>
2023-10-25 10:47:51 +0100

    style: tortee css style

---
+++ convo_starters.js
@@ -0,0 +1,27 @@
+const { v4: uuidv4 } = require('uuid');
+
+export const conversationStarters = {
+    question1: "What inspired you to start programming?",
+    question2: "Which programming languages are your favorites, and why?",
+    question3: "Have you ever worked on an open-source project?",
+    question4: "What's the most challenging bug you've ever had to fix?",
+    question5: "Do you prefer front-end or back-end development, and why?",
+    question6: "What's your favorite development tool or IDE?",
+    question7: "Have you ever built a mobile app, and if so, which platform did you use?",
+    question8: "How do you stay up-to-date with the latest programming trends and technologies?",
+    question9: "What's the most exciting project you've worked on recently?",
+    question10: "Do you have any programming pet peeves or coding practices you can't stand?",
+    question11: "Have you ever mentored or taught someone to code? What was that experience like?",
+    question12: "What's your preferred method for debugging code?",
+    question13: "Are there any programming languages or technologies you're interested in learning in the future?",
+    question14: "How do you handle burnout or stay motivated during long coding sessions?",
+    question15: "What's your opinion on the future of AI and machine learning in programming?",
+    question16: "What's the most valuable lesson you've learned in your programming career so far?",
+    question17: "Do you enjoy attending tech conferences or meetups, and if so, which ones have you found most beneficial?",
+    question18: "How do you balance work and life as a programmer?",
+    question19: "What's the best piece of advice you'd give to someone starting their programming journey?",
+    question20: "If you could build any software application, what would it be?"
+};
+
+const newUUID = uuidv4();
+console.log('Generated UUID:', newUUID);

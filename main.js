01d1363aae8089b596d69c38f91fa7c965da512b
Cal Woodford <callum@woodford.info>
2023-10-25 11:16:27 +0100

    added: numbering

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import { conversationStarters } from './convo_starters.js';
-import { v4 as uuidv4 } from 'uuid';
 
 console.log(conversationStarters);
 let buttonText = document.getElementById("apodButton")

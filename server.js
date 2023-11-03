1972f7d62b93ff8f785424b50eb04b904bf912d0
142804583+alexjbrowntech@users.noreply.github.com <142804583+alexjbrowntech@users.noreply.github.com>
2023-11-03 09:56:13 +0000

    updated port ref

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { app } from "./app.js";
 
-const PORT = process.env.PORT ?? 4001;
+const PORT = process.env.PORT ?? 4000;
 const HOST = process.env.HOST ?? "0.0.0.0";
 
 app.listen(PORT, HOST, function () {
